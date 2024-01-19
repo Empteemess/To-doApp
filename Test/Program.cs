5a09234f1086d867e13af303f0abf85a47594f90
giorgi <bazadzegiorgi9@gmail.com>
2024-01-19 20:11:50 +0400

    new changes

--- Test/Program.cs
+++ Test/Program.cs
@@ -2,9 +2,27 @@
 
 class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        GetName();
         Console.WriteLine("Hello");
+    }
+
+    public static void GetName()
+    {
+        Console.Write("Enter a name : ");
+        var input = Console.ReadLine();
+        
+        switch (input)
+        {
+          case  "gio":
+              
+              Console.WriteLine("bazadze");
+          break;
+          case "kaladze":
+              Console.WriteLine("Nadziralaaaaaa");
+              break;
+        }
     }
 }
