70c3556b4cf3a417e31fbd08734b3f13ae6ba012
giorgi <bazadzegiorgi9@gmail.com>
2024-02-17 15:02:41 +0400

    sad

--- PawnShop/Program.cs
+++ PawnShop/Program.cs
@@ -7,22 +7,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        var num1 = 12;
-        var num2 = 23;
-        (int sum, int average, string print) test = AddNumber(num1,num2);
-
-        Console.WriteLine(test.print);
-        Console.WriteLine($"Returned Sum is : {test.sum}");
-        Console.WriteLine($"Returned Average is is : {test.average}");
-    }
-
-
-    public static (int,int,string) AddNumber(int num1, int num2)
-    {
-        var sum = num1 + num2;
-        var average = (num1 + num2) / 2;
-        var PrintDetails = $"Sum is {sum} , Average is {average}";
-
-        return (sum, average, PrintDetails);
+            
     }
 }
+
+
+    
